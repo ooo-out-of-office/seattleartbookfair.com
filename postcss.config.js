ed29c90a9725cfcc8b925005aa4e9fbec073b8c5
Zack Seuberling <seuberling@gmail.com>
2020-05-04 00:24:06 -0700

    refactor postcss config for nextjs

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: ["./pages/**/*.js", "./components/**/*.js"],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
-const cssnano = require("cssnano");
-
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.COMPRESS_ASSETS === "true" ? [cssnano, purgecss] : []),
-  ],
+  plugins: {
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: process.env.COMPRESS_ASSETS === "true" ? {} : false,
+    "@fullhuman/postcss-purgecss":
+      process.env.COMPRESS_ASSETS === "true"
+        ? {
+            // Specify the paths to all of the template files in your project
+            content: ["./pages/**/*.js", "./components/**/*.js"],
+
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+  },
 };
