f7ae4ad0329d677986a4cf800d2ac8884e7441e7
Zack Seuberling <seuberling@gmail.com>
2020-05-03 23:56:43 -0700

    add next-offline package

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-module.exports = {
+const withOffline = require("next-offline");
+
+module.exports = withOffline({
   exportTrailingSlash: true,
   exportPathMap: function() {
     return {
       "/": { page: "/" },
       "/exhibitors": { page: "/exhibitors" },
       "/sponsorship": { page: "/sponsorship" },
-      "/programs": { page: "/programs" }
+      "/programs": { page: "/programs" },
     };
-  }
-};
+  },
+});
